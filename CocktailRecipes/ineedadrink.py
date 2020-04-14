962f76d4699601f9032810ba96a047f5019c7b0b
danny32352 <danjrooney@hotmail.com>
2020-04-14 23:14:28 +0100

    Trying to reload url to specify non-alcoholic or alcoholic drink.

--- CocktailRecipes/ineedadrink.py
+++ CocktailRecipes/ineedadrink.py
@@ -17,15 +17,23 @@ recipe = json.loads(r.content)
 data = list(recipe.items())[0][1][0]
 
 if choice is 'a' or 'A':
-    while str(data['strAlcoholic']) == 'Non Alcoholic':
-        r = requests.get(url)
-        recipe = json.loads(r.content)
-        data = list(recipe.items())[0][1][0]
+    while True:
+        if str(data['strAlcoholic']) == 'Alcoholic':
+            break
+        else:
+            url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+            r = requests.get(url)
+            recipe = json.loads(r.content)
+            data = list(recipe.items())[0][1][0]
 else:
-    while str(data['strAlcoholic']) == 'Alcoholic':
-        r = requests.get(url)
-        recipe = json.loads(r.content)
-        data = list(recipe.items())[0][1][0]
+    while True:
+        if str(data['strAlcoholic']) == 'Non alcoholic':
+            break
+        else:
+            url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+            r = requests.get(url)
+            recipe = json.loads(r.content)
+            data = list(recipe.items())[0][1][0]
 
 
 def number_of_ingredients():
